81b62ff5cf0a3a0c8623dec08fcbd745a589f6b4
abdulmajeed9t7-max <abdulmajeed9t7@gmail.com>
2026-09-23 13:07:18 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'shopvana-v1';
+const urlsToCache = [
+  '/Shopvana/',
+  '/Shopvana/index.html',
+  '/Shopvana/style.css',
+  '/Shopvana/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n))
+      );
+    })
+  );
+});
